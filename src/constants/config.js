3a88a3c7986ce999b545ea7917e62663319519d6
dariogguillen <dar_fe@hotmail.com>
2018-06-27 23:41:52 -0500

    modificando url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,4 @@
-export const WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather'
-export const FORECAST_URL = 'http://api.openweathermap.org/data/2.5/forecast'
+export const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather'
+export const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast'
+// export const KEY = '1d62f63cc3b10b215a826c839c162551'
 export const KEY = '72f2c5d3c27878e5f068f06388a39c3f'
