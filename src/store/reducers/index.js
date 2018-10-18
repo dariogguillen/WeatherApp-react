70085cf6490463cd8d3ce7b8951b3dc5c3cc6637
dariogguillen <dar_fe@hotmail.com>
2018-10-17 23:02:18 -0500

    utilizando libreria reselect

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import { createSelector } from 'reselect'
 import { city } from './city'
 import {
   cities,
@@ -7,6 +8,10 @@ import {
 
 export default combineReducers({ cities, city })
 
-export const getCity = state => state.city
+export const getCity = createSelector(state => state.city, city => city)
 
-export const getForecastDataFromCities = state => _getForecastDataFromCities(state.cities, getCity(state))
+export const getForecastDataFromCities = createSelector(
+  state => state.cities,
+  getCity,
+  _getForecastDataFromCities
+)
